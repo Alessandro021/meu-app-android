eff845f4616b9cfbbb0a70a081205a99cbc345d8
alessandro <alessandrodamata6@gmail.com>
2024-03-02 20:56:06 -0300

    feat: Constraint Layout na prática

--- app/src/main/java/com/cennaapp/meu_app/MainActivity.kt
+++ app/src/main/java/com/cennaapp/meu_app/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.replicando_tela_relativelayout)
+        setContentView(R.layout.constraint_layout_na_pratica)
     }
 }
